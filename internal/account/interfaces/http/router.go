279a5a82b791906aa227fa40e18a5ebd90de4ff5
Ana Carolina <amorim.analrds@gmail.com>
2026-06-09 15:53:14 -0300

    test

--- internal/account/interfaces/http/router.go
+++ internal/account/interfaces/http/router.go
@@ -6,5 +6,19 @@ func NewRouter(h *Handlers) http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /account", h.GetAccount)
 	mux.HandleFunc("PATCH /account", h.UpdateAccount)
-	return mux
+	return corsMiddleware(mux)
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, PATCH, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Private-Network", "true")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
--- internal/observability/interfaces/http/router.go
+++ internal/observability/interfaces/http/router.go
@@ -21,7 +21,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Private-Network", "true")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
